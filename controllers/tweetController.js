c65465023d6681ab9a85fad027409506420d9e05
leey611 <leey611@newschool.edu>
2020-07-24 11:54:01 -0400

    fix tweetReplyCounts

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -11,9 +11,11 @@ const tweetController = {
       include: [
         User,
         { model: User, as: 'TweetWhoLike' },
-        { model: User, as: 'whoReply' }
+        { model: User, as: 'whoReply' },
+        Reply
       ]
-    })
+    });
+    //console.log('tweets====================', tweets);
     data = tweets.map((r) => ({
       ...r.dataValues,
       userId: r.User.id,
@@ -23,8 +25,8 @@ const tweetController = {
       description: r.description,
       createdA: r.createdAt,
       likeCount: r.TweetWhoLike.length,
-      replayCount: r.whoReply.length,
-      isLiked: r.TweetWhoLike.map(d => d.id).includes(req.user.id)
+      replayCount: r.Replies.length,
+      isLiked: r.TweetWhoLike.map((d) => d.id).includes(req.user.id)
     }));
     return res.render('tweetsHome', { tweets: data });
   },
@@ -32,20 +34,21 @@ const tweetController = {
     const id = req.params.id;
     let tweet = await Tweet.findOne({
       where: { id },
-      include: [User, 
-        { model: User, as: 'TweetWhoLike'},
-        { model: User, as: 'whoReply', order: ["createdAt", 'DESC'] },
+      include: [
+        User,
+        { model: User, as: 'TweetWhoLike' },
+        { model: User, as: 'whoReply', order: ['createdAt', 'DESC'] }
       ]
     });
-    tweet = tweet.toJSON()
-   
-    let replies = tweet.whoReply
+    tweet = tweet.toJSON();
+
+    let replies = tweet.whoReply;
     const totalCount = {
       replyCount: replies.length,
       likeCount: tweet.TweetWhoLike.length,
-      isLiked: tweet.TweetWhoLike.map(d => d.id).includes(req.user.id)
-    }
-    console.log(totalCount)
+      isLiked: tweet.TweetWhoLike.map((d) => d.id).includes(req.user.id)
+    };
+    console.log(totalCount);
 
     res.render('tweet', { tweet, totalCount });
   },
@@ -57,27 +60,25 @@ const tweetController = {
     return Tweet.create({
       UserId: req.user.id,
       description: req.body.newTweet
-    })
-      .then((tweet) => {
-        req.flash('success_messages', '推文成功!!!')
-        res.redirect('/tweets')
-      })
+    }).then((tweet) => {
+      req.flash('success_messages', '推文成功!!!');
+      res.redirect('/tweets');
+    });
   },
   postComment: (req, res) => {
-    let whichTweet = req.params.id
+    let whichTweet = req.params.id;
     if (!req.body.newComment) {
-      req.flash('error_messages', "請輸入推文內容!!!")
-      return res.redirect('back')
+      req.flash('error_messages', '請輸入推文內容!!!');
+      return res.redirect('back');
     }
     return Reply.create({
       comment: req.body.newComment,
       TweetId: whichTweet,
       UserId: req.user.id
-    })
-      .then((tweet) => {
-        req.flash('success_messages', '回覆成功!!!')
-        res.redirect('/tweets')
-      })
-  },
+    }).then((tweet) => {
+      req.flash('success_messages', '回覆成功!!!');
+      res.redirect('/tweets');
+    });
+  }
 };
 module.exports = tweetController;
--- models/reply.js
+++ models/reply.js
@@ -1,12 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {
-    UserId: DataTypes.INTEGER,
-    TweetId: DataTypes.INTEGER,
-    comment: DataTypes.STRING
-  }, {});
-  Reply.associate = function(models) {  
-    
+  const Reply = sequelize.define(
+    'Reply',
+    {
+      UserId: DataTypes.INTEGER,
+      TweetId: DataTypes.INTEGER,
+      comment: DataTypes.STRING
+    },
+    {}
+  );
+  Reply.associate = function (models) {
+    Reply.belongsTo(models.Tweet);
   };
   return Reply;
 };
